b0387bde5d40c880cd911719c9b36390115cb265
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2014-06-06 12:38:52 +0200

    add make_unique

---
+++ include/qbb/util/make_unique.hpp
@@ -0,0 +1,20 @@
+#ifndef QBB_UTIL_MAKE_UNIQUE_HPP
+#define QBB_UTIL_MAKE_UNIQUE_HPP
+
+#include <utility>
+
+namespace qbb
+{
+namespace util
+{
+    
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+}
+}
+
+#endif
