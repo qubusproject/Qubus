def9c2f0796c4b6a5a820f50e0088e27992278ab
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2015-10-31 18:39:35 +0100

    Add more tests for complex arithmetic.

--- qubus/tests/basic_expressions.cpp
+++ qubus/tests/basic_expressions.cpp
@@ -55,6 +55,170 @@ TEST(basic_expressions, constant_expr)
     ASSERT_NEAR(error, 0.0, 1e-14);
 }
 
+TEST(basic_expressions, complex_addition)
+{
+    using namespace qbb::qubus;
+
+    long int N = 100;
+
+    std::random_device rd;
+
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    std::vector<std::complex<double>> A2(N);
+    std::vector<std::complex<double>> B2(N);
+    std::vector<std::complex<double>> C2(N);
+
+    for (long int i = 0; i < N; ++i)
+    {
+        A2[i] = std::complex<double>(dist(gen), dist(gen));
+        B2[i] = std::complex<double>(dist(gen), dist(gen));
+    }
+
+    qbb::qubus::index i("i");
+
+    tensor<std::complex<double>, 1> A(N);
+    tensor<std::complex<double>, 1> B(N);
+    tensor<std::complex<double>, 1> C(N);
+
+    auto init_A = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> A)
+                  {
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          A(i) = A2[i];
+                      }
+                  })
+            .finalize();
+
+    auto init_B = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> B)
+                  {
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          B(i) = B2[i];
+                      }
+                  })
+            .finalize();
+
+    execute(init_A, A);
+    execute(init_B, B);
+
+    tensor_expr<std::complex<double>, 1> Cdef = def_tensor(i)[A(i) + B(i)];
+
+    C = Cdef;
+
+    for (long int i = 0; i < N; ++i)
+    {
+        C2[i] = A2[i] + B2[i];
+    }
+
+    double error;
+
+    auto test = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> C)
+                  {
+                      error = 0.0;
+
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          std::complex<double> diff = C(i) - C2[i];
+
+                          error += std::norm(diff);
+                      }
+                  })
+            .finalize();
+
+    execute(test, C);
+    C.when_ready().wait();
+
+    ASSERT_NEAR(error, 0.0, 1e-14);
+}
+
+TEST(basic_expressions, complex_substraction)
+{
+    using namespace qbb::qubus;
+
+    long int N = 100;
+
+    std::random_device rd;
+
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    std::vector<std::complex<double>> A2(N);
+    std::vector<std::complex<double>> B2(N);
+    std::vector<std::complex<double>> C2(N);
+
+    for (long int i = 0; i < N; ++i)
+    {
+        A2[i] = std::complex<double>(dist(gen), dist(gen));
+        B2[i] = std::complex<double>(dist(gen), dist(gen));
+    }
+
+    qbb::qubus::index i("i");
+
+    tensor<std::complex<double>, 1> A(N);
+    tensor<std::complex<double>, 1> B(N);
+    tensor<std::complex<double>, 1> C(N);
+
+    auto init_A = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> A)
+                  {
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          A(i) = A2[i];
+                      }
+                  })
+            .finalize();
+
+    auto init_B = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> B)
+                  {
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          B(i) = B2[i];
+                      }
+                  })
+            .finalize();
+
+    execute(init_A, A);
+    execute(init_B, B);
+
+    tensor_expr<std::complex<double>, 1> Cdef = def_tensor(i)[A(i) - B(i)];
+
+    C = Cdef;
+
+    for (long int i = 0; i < N; ++i)
+    {
+        C2[i] = A2[i] - B2[i];
+    }
+
+    double error;
+
+    auto test = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> C)
+                  {
+                      error = 0.0;
+
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          std::complex<double> diff = C(i) - C2[i];
+
+                          error += std::norm(diff);
+                      }
+                  })
+            .finalize();
+
+    execute(test, C);
+    C.when_ready().wait();
+
+    ASSERT_NEAR(error, 0.0, 1e-14);
+}
+
 TEST(basic_expressions, complex_multiplication)
 {
     using namespace qbb::qubus;
@@ -130,6 +294,88 @@ TEST(basic_expressions, complex_multiplication)
                               }
                           })
                     .finalize();
+
+    execute(test, C);
+    C.when_ready().wait();
+
+    ASSERT_NEAR(error, 0.0, 1e-14);
+}
+
+TEST(basic_expressions, complex_division)
+{
+    using namespace qbb::qubus;
+
+    long int N = 100;
+
+    std::random_device rd;
+
+    std::mt19937 gen(rd());
+
+    std::uniform_real_distribution<double> dist(-10.0, 10.0);
+
+    std::vector<std::complex<double>> A2(N);
+    std::vector<std::complex<double>> B2(N);
+    std::vector<std::complex<double>> C2(N);
+
+    for (long int i = 0; i < N; ++i)
+    {
+        A2[i] = std::complex<double>(dist(gen), dist(gen));
+        B2[i] = std::complex<double>(dist(gen), dist(gen));
+    }
+
+    qbb::qubus::index i("i");
+
+    tensor<std::complex<double>, 1> A(N);
+    tensor<std::complex<double>, 1> B(N);
+    tensor<std::complex<double>, 1> C(N);
+
+    auto init_A = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> A)
+                  {
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          A(i) = A2[i];
+                      }
+                  })
+            .finalize();
+
+    auto init_B = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> B)
+                  {
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          B(i) = B2[i];
+                      }
+                  })
+            .finalize();
+
+    execute(init_A, A);
+    execute(init_B, B);
+
+    tensor_expr<std::complex<double>, 1> Cdef = def_tensor(i)[A(i) / B(i)];
+
+    C = Cdef;
+
+    for (long int i = 0; i < N; ++i)
+    {
+        C2[i] = A2[i] / B2[i];
+    }
+
+    double error;
+
+    auto test = make_plan()
+            .body([&](cpu_tensor_view<std::complex<double>, 1> C)
+                  {
+                      error = 0.0;
+
+                      for (long int i = 0; i < N; ++i)
+                      {
+                          std::complex<double> diff = C(i) - C2[i];
+
+                          error += std::norm(diff);
+                      }
+                  })
+            .finalize();
 
     execute(test, C);
     C.when_ready().wait();
