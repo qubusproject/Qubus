29080ae7d3f1961db0357bb6eb3b7291e09a4180
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2016-12-29 22:05:15 +0100

    Add an analysis pass to prove the invariance of an expression

---
+++ qubus/src/task_invariants_analysis.cpp
@@ -0,0 +1,126 @@
+#include <qbb/qubus/task_invariants_analysis.hpp>
+
+#include <qbb/qubus/alias_analysis.hpp>
+
+#include <qbb/qubus/pattern/IR.hpp>
+#include <qbb/qubus/pattern/core.hpp>
+
+#include <qbb/util/assert.hpp>
+
+namespace qbb
+{
+namespace qubus
+{
+
+namespace
+{
+bool is_referencing_invariant_object(const expression& obj, const expression& context,
+                                     const basic_alias_analysis_result& basic_alias_analysis)
+{
+    using pattern::_;
+    using pattern::value;
+
+    pattern::variable<const expression&> modified_object;
+    pattern::variable<variable_declaration> var;
+
+    bool is_modified = false;
+
+    auto m =
+        pattern::make_matcher<expression, void>()
+            .case_(assign(modified_object, _) || plus_assign(modified_object, _),
+                   [&] {
+                       const access_expr* modifing_access =
+                           modified_object.get().try_as<access_expr>();
+                       const access_expr* obj_access = obj.try_as<access_expr>();
+
+                       QBB_ASSERT(modifing_access && obj_access, "Invalid object during access.");
+
+                       auto query_result = basic_alias_analysis.alias(access(*obj_access),
+                                                                      access(*modifing_access));
+
+                       if (query_result != alias_result::noalias)
+                       {
+                           is_modified = true;
+                       }
+                   })
+            .case_(for_(var, _, _, _, _), [&] {
+                auto modifing_access = variable_ref(var.get());
+                const access_expr* obj_access = obj.try_as<access_expr>();
+
+                QBB_ASSERT(obj_access, "Invalid object during access.");
+
+                // FIXME: Currently, we use the non-caching variant of the basic alias analysis algorithm
+                //        since the cache gets confused by temporary expression objects.
+                auto query_result =
+                        alias_using_basic_rules(access(*obj_access), access(*modifing_access));
+
+                if (query_result != alias_result::noalias)
+                {
+                    is_modified = true;
+                }
+            });
+
+    pattern::for_each(context, m);
+
+    return !is_modified;
+}
+}
+
+task_invariants_analysis_result::task_invariants_analysis_result(
+    const basic_alias_analysis_result& basic_alias_analysis_)
+: basic_alias_analysis_(&basic_alias_analysis_)
+{
+}
+
+bool task_invariants_analysis_result::is_invariant(const expression& expr,
+                                                   const expression& context)
+{
+    using pattern::_;
+
+    auto m =
+        pattern::make_matcher<expression, bool>()
+            .case_(pattern::access(),
+                   [&](const expression& self) {
+                       return is_referencing_invariant_object(self, context,
+                                                              *basic_alias_analysis_);
+                   })
+            // Literals are invariant by definition
+            .case_(double_literal(_) || float_literal(_) || integer_literal(_), [] { return true; })
+            // Special rule for array shapes
+            // TODO: We should generalize this. Maybe with a special type attribute
+            // TODO: We should test if the object is actually a built-in array. For now it should be fine, though.
+            .case_(member_access(_, pattern::value("shape")), [&] { return true; })
+            // The following operations are all pure functions. Their results are invariant if all
+            // their arguments are invariants themselves.
+            .case_(binary_operator(_, _, _) || unary_operator(_, _) || member_access(_, _) ||
+                       subscription(_, _),
+                   [&](const expression& self) {
+                       for (const auto& child : self.sub_expressions())
+                       {
+                           if (!is_invariant(child, context))
+                               return false;
+                       }
+
+                       return true;
+                   })
+            .case_(_, [] { return false; });
+
+    return pattern::match(expr, m);
+}
+
+task_invariants_analysis_result
+task_invariants_analysis_pass::run(const expression& root, analysis_manager& manager,
+                                   pass_resource_manager& resource_manager) const
+{
+
+    return task_invariants_analysis_result(manager.get_analysis<basic_alias_analysis_pass>(root));
+}
+
+std::vector<analysis_id> task_invariants_analysis_pass::required_analyses() const
+{
+    return {get_analysis_id<basic_alias_analysis_pass>()};
+}
+
+QUBUS_REGISTER_ANALYSIS_PASS(task_invariants_analysis_pass);
+}
+}
---
+++ qubus/tests/task_invariants_analysis.cpp
@@ -0,0 +1,61 @@
+#include <qbb/qubus/qubus.hpp>
+
+#include <qbb/qubus/IR/qir.hpp>
+#include <qbb/qubus/task_invariants_analysis.hpp>
+
+#include <hpx/hpx_init.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(task_invariants_analysis, loop_bound_and_ind_var)
+{
+    using namespace qbb::qubus;
+
+    variable_declaration var(types::array(types::double_{}));
+
+    variable_declaration i(types::integer{});
+    variable_declaration j(types::integer{});
+
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(variable_ref(i));
+
+    auto access = subscription(variable_ref(var), std::move(indices));
+
+    auto upper_bound = subscription(member_access(variable_ref(var), "shape"), integer_literal(0));
+
+    auto root = for_(i, integer_literal(0), clone(*upper_bound),
+                     for_(j, integer_literal(0), clone(*upper_bound), std::move(access)));
+
+    const auto& bound = root->body().child(1);
+    const auto& induction_var = root->body().child(3).child(1);
+    const auto& second_loop = root->body();
+
+    pass_resource_manager resource_man;
+    analysis_manager analysis_man(resource_man);
+
+    task_invariants_analysis_pass analysis;
+
+    auto result = analysis.run(*root, analysis_man, resource_man);
+
+    EXPECT_TRUE(result.is_invariant(bound, *root));
+    EXPECT_FALSE(result.is_invariant(induction_var, *root));
+    EXPECT_TRUE(result.is_invariant(induction_var, second_loop));
+}
+
+int hpx_main(int argc, char** argv)
+{
+    qbb::qubus::init(argc, argv);
+
+    auto result = RUN_ALL_TESTS();
+
+    hpx::finalize();
+
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return hpx::init(argc, argv);
+}
