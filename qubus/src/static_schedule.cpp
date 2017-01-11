7181c49f27b774992484c9cf3b22568e941a1144
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2017-01-11 14:06:26 +0100

    Implement the static schedule analysis pass
    The analysis tries to find parts of the schedule given by the IR  whose structure is
    completely determined at compile time (thus static).
    
    The static structure allows us to reason about these parts in a much
    more elaborate manner. For example, to prove that a certain reordering
    of tasks is valid is much easier in this representation.  As a side effect,
    we can easily prove that a set of tasks can be executed concurrently
    (All permutations are valid).

---
+++ qubus/src/static_schedule.cpp
@@ -0,0 +1,51 @@
+#include <qbb/qubus/static_schedule.hpp>
+
+#include <algorithm>
+#include <utility>
+
+namespace qbb
+{
+namespace qubus
+{
+static_schedule_statement::static_schedule_statement(std::string name, const expression& expr)
+: name(std::move(name)), expr(expr)
+{
+}
+
+static_schedule_context::static_schedule_context(std::shared_ptr<affine_expr_context> affine_ctx_)
+: affine_ctx_(std::move(affine_ctx_))
+{
+}
+
+static_schedule_statement& static_schedule_context::register_statement(const expression& stmt)
+{
+    auto name = "stmt" + std::to_string(free_statement_id_++);
+
+    return statement_table_.emplace(name, static_schedule_statement(name, stmt)).first->second;
+}
+
+affine_expr_context& static_schedule_context::affine_ctx()
+{
+    return *affine_ctx_;
+}
+
+static_schedule::static_schedule(std::unordered_map<const expression*, isl::schedule_node> ir_schedule_map_)
+: ir_schedule_map_(std::move(ir_schedule_map_))
+{
+}
+
+const isl::schedule_node& static_schedule::get_schedule_node(const expression& expr) const
+{
+    auto search_result = ir_schedule_map_.find(&expr);
+
+    if (search_result != ir_schedule_map_.end())
+    {
+        return search_result->second;
+    }
+    else
+    {
+        throw 0; // Error: Invalid expression
+    }
+}
+}
+}
---
+++ qubus/tests/static_schedule_analysis.cpp
@@ -0,0 +1,226 @@
+#include <qbb/qubus/qubus.hpp>
+
+#include <qbb/qubus/IR/qir.hpp>
+#include <qbb/qubus/static_schedule_analysis.hpp>
+
+#include <hpx/hpx_init.hpp>
+
+#include <gtest/gtest.h>
+
+TEST(static_schedule_analysis, empty_loop)
+{
+    using namespace qbb::qubus;
+
+    variable_declaration i(types::integer{});
+
+    auto loop = for_(i, integer_literal(0), integer_literal(42),
+                     if_(less(var(i), integer_literal(21)), sequenced_tasks({})));
+
+    auto& body = loop->body();
+
+    pass_resource_manager resource_manager;
+    analysis_manager analysis_man(resource_manager);
+
+    static_schedule_analysis_pass analysis;
+
+    auto result = analysis.run(*loop, analysis_man, resource_manager);
+
+    auto schedule = result.get_schedule_containing(body);
+
+    ASSERT_TRUE(schedule);
+
+    auto node = schedule->get_schedule_node(*loop);
+
+    auto schedule_map = node.get_subtree_schedule_union_map();
+
+    auto empty_map = isl::union_map::empty(schedule_map.get_space());
+
+    EXPECT_EQ(schedule_map, empty_map);
+}
+
+TEST(static_schedule_analysis, zero_init)
+{
+    using namespace qbb::qubus;
+
+    variable_declaration i(types::integer{});
+
+    variable_declaration A(types::array(types::double_{}));
+
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(i));
+
+    auto loop = for_(i, integer_literal(0), integer_literal(42),
+                     assign(subscription(var(A), std::move(indices)), integer_literal(0)));
+
+    auto& body = loop->body();
+
+    pass_resource_manager resource_manager;
+    analysis_manager analysis_man(resource_manager);
+
+    auto& isl_ctx = resource_manager.get_isl_ctx();
+
+    static_schedule_analysis_pass analysis;
+
+    auto result = analysis.run(*loop, analysis_man, resource_manager);
+
+    auto schedule = result.get_schedule_containing(body);
+
+    ASSERT_TRUE(schedule);
+
+    auto node = schedule->get_schedule_node(*loop);
+
+    auto schedule_map = node.get_subtree_schedule_union_map();
+
+    auto expected_schedule_map =
+        isl::union_map(isl_ctx, "{ stmt0[i0] -> [i0] : i0 >= 0 and i0 <= 41 }");
+
+    EXPECT_EQ(schedule_map, expected_schedule_map);
+}
+
+TEST(static_schedule_analysis, strip_mined_loop)
+{
+    using namespace qbb::qubus;
+
+    variable_declaration i(types::integer{});
+    variable_declaration ii(types::integer{});
+    variable_declaration N(types::integer{});
+
+    variable_declaration A(types::array(types::double_{}));
+
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(ii));
+
+    auto loop = for_(i, integer_literal(0), var(N), integer_literal(3),
+                     for_(ii, var(i), var(i) + integer_literal(3),
+                          assign(subscription(var(A), std::move(indices)), integer_literal(0))));
+
+    auto& body = loop->body();
+
+    pass_resource_manager resource_manager;
+    analysis_manager analysis_man(resource_manager);
+
+    auto& isl_ctx = resource_manager.get_isl_ctx();
+
+    static_schedule_analysis_pass analysis;
+
+    auto result = analysis.run(*loop, analysis_man, resource_manager);
+
+    auto schedule = result.get_schedule_containing(body);
+
+    ASSERT_TRUE(schedule);
+
+    auto node = schedule->get_schedule_node(*loop);
+
+    auto schedule_map = node.get_subtree_schedule_union_map();
+
+    auto expected_schedule_map = isl::union_map(
+        isl_ctx, "[p0] -> { stmt0[i0, i1] -> [i0, i1] : exists (e0 = floor((i0)/3): 3e0 = i0 and "
+                 "i0 >= 0 and i0 < p0 and i1 >= i0 and i1 <= 2 + i0) }");
+
+    EXPECT_EQ(schedule_map, expected_schedule_map);
+}
+
+TEST(static_schedule_analysis, multiple_statements)
+{
+    using namespace qbb::qubus;
+
+    variable_declaration i(types::integer{});
+    variable_declaration N(types::integer{});
+
+    variable_declaration A(types::array(types::double_{}));
+
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(i));
+
+    std::vector<std::unique_ptr<expression>> tasks;
+    tasks.push_back(assign(subscription(var(A), std::move(indices)), integer_literal(0)));
+    tasks.push_back(assign(subscription(var(A), std::move(indices)), integer_literal(42)));
+
+    auto loop = for_(i, integer_literal(0), var(N), sequenced_tasks(std::move(tasks)));
+
+    auto& body = loop->body();
+
+    pass_resource_manager resource_manager;
+    analysis_manager analysis_man(resource_manager);
+
+    auto& isl_ctx = resource_manager.get_isl_ctx();
+
+    static_schedule_analysis_pass analysis;
+
+    auto result = analysis.run(*loop, analysis_man, resource_manager);
+
+    auto schedule = result.get_schedule_containing(body);
+
+    ASSERT_TRUE(schedule);
+
+    auto node = schedule->get_schedule_node(*loop);
+
+    auto schedule_map = node.get_subtree_schedule_union_map();
+
+    auto expected_schedule_map =
+        isl::union_map(isl_ctx, "[p0] -> { stmt0[i0] -> [i0, 0] : i0 >= 0 and i0 < p0; "
+                                "stmt1[i0] -> [i0, 1] : i0 >= 0 and i0 < p0 }");
+
+    EXPECT_EQ(schedule_map, expected_schedule_map);
+}
+
+TEST(static_schedule_analysis, loop_with_guard)
+{
+    using namespace qbb::qubus;
+
+    variable_declaration i(types::integer{});
+    variable_declaration N(types::integer{});
+
+    variable_declaration A(types::array(types::double_{}));
+
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(i));
+
+    auto loop = for_(i, integer_literal(0), var(N),
+                     if_(less(var(i), var(N) / integer_literal(2)),
+                         assign(subscription(var(A), std::move(indices)), integer_literal(0)),
+                         assign(subscription(var(A), std::move(indices)), integer_literal(42))));
+
+    auto& body = loop->body();
+
+    pass_resource_manager resource_manager;
+    analysis_manager analysis_man(resource_manager);
+
+    auto& isl_ctx = resource_manager.get_isl_ctx();
+
+    static_schedule_analysis_pass analysis;
+
+    auto result = analysis.run(*loop, analysis_man, resource_manager);
+
+    auto schedule = result.get_schedule_containing(body);
+
+    ASSERT_TRUE(schedule);
+
+    auto node = schedule->get_schedule_node(*loop);
+
+    auto schedule_map = node.get_subtree_schedule_union_map();
+
+    auto expected_schedule_map =
+        isl::union_map(isl_ctx, "[p0] -> { stmt0[i0] -> [i0, 0] : i0 >= 0 and 2i0 <= -2 + p0; "
+                                "stmt1[i0] -> [i0, 1] : i0 < p0 and 2i0 >= -1 + p0 }");
+
+    EXPECT_EQ(schedule_map, expected_schedule_map);
+}
+
+int hpx_main(int argc, char** argv)
+{
+    qbb::qubus::init(argc, argv);
+
+    auto result = RUN_ALL_TESTS();
+
+    hpx::finalize();
+
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return hpx::init(argc, argv);
+}
