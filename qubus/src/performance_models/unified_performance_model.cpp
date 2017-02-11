eb75ef5ccbb578b09a4e4af4d34c59eb419186da
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2017-02-11 14:20:32 +0100

    Add the first parts of the performance modelling infrastructure
    In addition to the interface, this commit also adds two implementations.
    
    The first implementation is a simple statistical model based on Boost Accumulators
    which uses the moments of a recorded frequency distribution for every kernel instance.
    
    The second implementation, the unified performance model, provides an abstraction
    over such concrete models and will allow us to pick a specific model based on
    the properties of a certain kernel. Specifically, I will add an advanced performance model
    in a followup commit for kernels with scalar parameters which will exploit this
    infrastructure.

---
+++ qubus/src/performance_models/unified_performance_model.cpp
@@ -0,0 +1,89 @@
+#include <qbb/qubus/performance_models/unified_performance_model.hpp>
+
+#include <qbb/qubus/performance_models/simple_statistical_performance_model.hpp>
+
+#include <hpx/include/local_lcos.hpp>
+
+#include <qbb/util/assert.hpp>
+
+#include <mutex>
+#include <unordered_map>
+#include <utility>
+
+namespace qbb
+{
+namespace qubus
+{
+class unified_performance_model_impl
+{
+public:
+    void sample_execution_time(const computelet& c, const execution_context& ctx,
+                               std::chrono::microseconds execution_time)
+    {
+        std::lock_guard<hpx::lcos::local::mutex> guard(models_mutex_);
+
+        auto kernel_id = c.id();
+
+        auto search_result = kernel_models_.find(kernel_id);
+
+        if (search_result == kernel_models_.end())
+        {
+            search_result =
+                kernel_models_
+                    .emplace(kernel_id, std::make_unique<simple_statistical_performance_model>())
+                    .first;
+        }
+
+        search_result->second->sample_execution_time(c, ctx, std::move(execution_time));
+    }
+
+    boost::optional<performance_estimate>
+    try_estimate_execution_time(const computelet& c, const execution_context& ctx) const
+    {
+        std::lock_guard<hpx::lcos::local::mutex> guard(models_mutex_);
+
+        auto kernel_id = c.id();
+
+        auto search_result = kernel_models_.find(kernel_id);
+
+        if (search_result != kernel_models_.end())
+        {
+            return search_result->second->try_estimate_execution_time(c, ctx);
+        }
+        else
+        {
+            return boost::none;
+        }
+    }
+
+private:
+    mutable hpx::lcos::local::mutex models_mutex_;
+    std::unordered_map<hpx::naming::gid_type, std::unique_ptr<performance_model>> kernel_models_;
+};
+
+unified_performance_model::unified_performance_model()
+: impl_(std::make_unique<unified_performance_model_impl>())
+{
+}
+
+unified_performance_model::~unified_performance_model() = default;
+
+void unified_performance_model::sample_execution_time(const computelet& c,
+                                                      const execution_context& ctx,
+                                                      std::chrono::microseconds execution_time)
+{
+    QBB_ASSERT(impl_, "Uninitialized object.");
+
+    impl_->sample_execution_time(c, ctx, std::move(execution_time));
+}
+
+boost::optional<performance_estimate>
+unified_performance_model::try_estimate_execution_time(const computelet& c,
+                                                       const execution_context& ctx) const
+{
+    QBB_ASSERT(impl_, "Uninitialized object.");
+
+    return impl_->try_estimate_execution_time(c, ctx);
+}
+}
+}
