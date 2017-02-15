efffb0b5e1cef80ad4abf00b79c2f7968c26f1e7
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2017-02-15 16:25:50 +0100

    Add a performance model based on symbolic regression
    The new model tries to find a suitable model function over time which
    characterizes the measured data. These models only depend
    on the scalar parameters of the kernel. Therefore, the
    overall performance model dispatches each operation
    to a partial model for each combination of non-scalar parameters
    to cover all cases.
    
    Currently, the model might consume a huge chunk of memory
    to store the data and the models. To avoid this, we have to
    restrict the amount of stored data to a sufficiently small value.
    Since this implies that we would need to discard some data eventually,
    we need to come forward with a good idea to categorize the data
    according to its importance before we actually implement such
    a limit.
    
    Since this is a more general problem which affects all performance
    models to some extent, the solution should also be generic enough
    to work for any performance model.

---
+++ qubus/src/performance_models/regression_performance_model.cpp
@@ -0,0 +1,200 @@
+#include <qbb/qubus/performance_models/regression_performance_model.hpp>
+
+#include <qbb/qubus/performance_models/symbolic_regression.hpp>
+
+#include <qbb/qubus/get_view.hpp>
+#include <qbb/qubus/host_object_views.hpp>
+
+#include <hpx/include/local_lcos.hpp>
+
+#include <qbb/util/assert.hpp>
+
+#include <map>
+#include <mutex>
+#include <utility>
+#include <vector>
+
+namespace qbb
+{
+namespace qubus
+{
+
+namespace
+{
+
+class regression_analysis
+{
+public:
+    regression_analysis() = default;
+
+    regression_analysis(const regression_analysis&) = delete;
+    regression_analysis& operator=(const regression_analysis&) = delete;
+
+    regression_analysis(regression_analysis&&) = default;
+    regression_analysis& operator=(regression_analysis&&) = default;
+
+    void add_datapoint(std::vector<double> arguments, std::chrono::microseconds execution_time)
+    {
+        regression_model_.add_datapoint(std::move(arguments), std::move(execution_time));
+
+        ++number_of_new_data_points_;
+
+        if (number_of_new_data_points_ > 10 && regression_model_.size_of_dataset() > 10)
+        {
+            regression_model_.update();
+
+            number_of_new_data_points_ = 0;
+        }
+    }
+
+    boost::optional<std::chrono::microseconds> query(const std::vector<double>& arguments) const
+    {
+        return regression_model_.query(arguments);
+    }
+
+    boost::optional<std::chrono::microseconds> accuracy() const
+    {
+        return regression_model_.accuracy();
+    }
+
+private:
+    symbolic_regression regression_model_;
+
+    long int number_of_new_data_points_ = 0;
+};
+
+std::vector<hpx::naming::gid_type> generate_key_from_ctx(const execution_context& ctx)
+{
+    std::vector<hpx::naming::gid_type> key;
+    key.reserve(ctx.args().size());
+
+    for (const auto& obj : ctx.args())
+    {
+        auto datatype = obj.object_type();
+
+        if (!(datatype == types::integer{} || datatype == types::float_{} ||
+              datatype == types::double_{}))
+        {
+            key.push_back(obj.get_raw_gid());
+        }
+    }
+
+    return key;
+}
+
+std::vector<double> extract_arguments_from_ctx(const execution_context& ctx)
+{
+    std::vector<double> arguments;
+    arguments.reserve(ctx.args().size());
+
+    for (const auto& obj : ctx.args())
+    {
+        auto datatype = obj.object_type();
+
+        if (datatype == types::integer{})
+        {
+            auto view = get_view<host_scalar_view<util::index_t>>(obj).get();
+
+            arguments.push_back(view.get());
+        }
+        else if (datatype == types::double_{})
+        {
+            auto view = get_view<host_scalar_view<double>>(obj).get();
+
+            arguments.push_back(view.get());
+        }
+        else if (datatype == types::float_{})
+        {
+            auto view = get_view<host_scalar_view<float>>(obj).get();
+
+            arguments.push_back(view.get());
+        }
+    }
+
+    return arguments;
+}
+}
+
+class regression_performance_model_impl
+{
+public:
+    void sample_execution_time(const computelet& QBB_UNUSED(c), const execution_context& ctx,
+                               std::chrono::microseconds execution_time)
+    {
+        std::lock_guard<hpx::lcos::local::mutex> guard(regression_analyses_mutex_);
+
+        auto key = generate_key_from_ctx(ctx);
+        auto arguments = extract_arguments_from_ctx(ctx);
+
+        auto search_result = regression_analyses_.find(key);
+
+        if (search_result == regression_analyses_.end())
+        {
+            search_result = regression_analyses_.emplace(key, regression_analysis()).first;
+        }
+
+        search_result->second.add_datapoint(std::move(arguments), std::move(execution_time));
+    }
+
+    boost::optional<performance_estimate>
+    try_estimate_execution_time(const computelet& QBB_UNUSED(c), const execution_context& ctx) const
+    {
+        std::lock_guard<hpx::lcos::local::mutex> guard(regression_analyses_mutex_);
+
+        auto key = generate_key_from_ctx(ctx);
+
+        auto search_result = regression_analyses_.find(key);
+
+        if (search_result != regression_analyses_.end())
+        {
+            auto arguments = extract_arguments_from_ctx(ctx);
+
+            auto query_result = search_result->second.query(arguments);
+            auto accuracy_result = search_result->second.accuracy();
+
+            if (query_result && accuracy_result)
+            {
+                return performance_estimate{*std::move(query_result), *std::move(accuracy_result)};
+            }
+            else
+            {
+                return boost::none;
+            }
+        }
+        else
+        {
+            return boost::none;
+        }
+    }
+
+private:
+    mutable hpx::lcos::local::mutex regression_analyses_mutex_;
+    std::map<std::vector<hpx::naming::gid_type>, regression_analysis> regression_analyses_;
+};
+
+regression_performance_model::regression_performance_model()
+: impl_(std::make_unique<regression_performance_model_impl>())
+{
+}
+
+regression_performance_model::~regression_performance_model() = default;
+
+void regression_performance_model::sample_execution_time(const computelet& c,
+                                                         const execution_context& ctx,
+                                                         std::chrono::microseconds execution_time)
+{
+    QBB_ASSERT(impl_, "Uninitialized object.");
+
+    impl_->sample_execution_time(c, ctx, std::move(execution_time));
+}
+
+boost::optional<performance_estimate>
+regression_performance_model::try_estimate_execution_time(const computelet& c,
+                                                          const execution_context& ctx) const
+{
+    QBB_ASSERT(impl_, "Uninitialized object.");
+
+    return impl_->try_estimate_execution_time(c, ctx);
+}
+}
+}
