274d6175497d7dfa4f3c582525a8054e37e2b3e2
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2017-04-02 23:49:54 +0200

    Add a thin wrapper library around the CUDA driver API

---
+++ qubus/include/qubus/cuda/core.hpp
@@ -0,0 +1,229 @@
+#ifndef QUBUS_CUDA_CORE_HPP
+#define QUBUS_CUDA_CORE_HPP
+
+#include <cuda.h>
+
+#include <exception>
+#include <functional>
+#include <list>
+#include <string>
+#include <vector>
+
+namespace qubus
+{
+namespace cuda
+{
+
+class exception : public virtual std::exception
+{
+};
+
+class cuda_error : public virtual exception
+{
+public:
+    explicit cuda_error(CUresult error_code_);
+
+    CUresult error_code() const;
+
+    const char* what() const noexcept override;
+private:
+    CUresult error_code_;
+};
+
+void check_cuda_error(CUresult result);
+
+void init();
+
+struct compute_capability_info
+{
+    compute_capability_info(int major_revision, int minor_revision)
+    : major_revision(major_revision), minor_revision(minor_revision)
+    {
+    }
+
+    int major_revision;
+    int minor_revision;
+};
+
+class device
+{
+public:
+    explicit device(int ordinal);
+
+    device(const device&) = delete;
+    device& operator=(const device&) = delete;
+
+    device(device&& other);
+    device& operator=(device&& other);
+
+    std::string name() const;
+
+    std::size_t total_mem() const;
+
+    compute_capability_info compute_capability() const;
+
+    int get_attribute(CUdevice_attribute attribute) const;
+
+    int max_threads_per_block() const;
+    int max_shared_memory_per_block() const;
+    int warp_size() const;
+    int max_block_dim_x() const;
+    int max_grid_dim_x() const;
+
+    CUdevice native_handle() const;
+
+private:
+    CUdevice device_;
+};
+
+int get_device_count();
+std::vector<device> get_devices();
+
+class context
+{
+public:
+    context();
+
+    explicit context(const device& dev);
+
+    context(const context&) = delete;
+    context& operator=(const context&) = delete;
+
+    context(context&& other);
+
+    context& operator=(context&& other);
+
+    ~context();
+
+    CUcontext native_handle() const;
+
+private:
+    CUcontext context_;
+};
+
+namespace this_context
+{
+void synchronize();
+}
+
+class stream
+{
+public:
+    using callback_closure = std::function<void()>;
+
+    stream();
+
+    stream(const stream&) = delete;
+    stream& operator=(const stream&) = delete;
+
+    stream(stream&& other);
+
+    stream& operator=(stream&& other);
+
+    ~stream();
+
+    void add_callback(std::function<void()> callback);
+
+    CUstream native_handle() const;
+
+private:
+    CUstream stream_handle_;
+    std::list<stream::callback_closure> pending_callbacks_;
+};
+
+class function
+{
+public:
+    explicit function(CUfunction function_);
+
+    CUfunction native_handle() const;
+
+private:
+    CUfunction function_;
+};
+
+template <typename... Parameters>
+void launch_kernel(const function& kernel, std::size_t grid_extend, std::size_t block_extend,
+                   std::size_t shared_memory_size, Parameters&&... parameters)
+{
+    void* params[] = {&parameters...};
+
+    cuLaunchKernel(kernel.native_handle(), grid_extend, 1, 1, block_extend, 1, 1,
+                   shared_memory_size, 0, params, 0);
+}
+
+int calculate_max_active_blocks_per_multiprocessor(const function& kernel, int block_size,
+                                                   std::size_t dynamic_shared_mem_size);
+
+struct suggested_launch_config
+{
+    suggested_launch_config(int min_grid_size, int block_size)
+    : min_grid_size(min_grid_size), block_size(block_size)
+    {
+    }
+
+    int min_grid_size;
+    int block_size;
+};
+
+suggested_launch_config
+calculate_launch_config_with_max_occupancy(const function& kernel,
+                                           std::size_t dynamic_shared_mem_size);
+
+class module
+{
+public:
+    explicit module(const std::string& ptx_code);
+
+    module(const module&) = delete;
+    module& operator=(const module&) = delete;
+
+    module(module&& other);
+    module& operator=(module&& other);
+
+    ~module();
+
+    const std::string& log_message() const;
+    const std::string& error_message() const;
+
+    function get_function(const std::string& name) const;
+
+    CUmodule native_handle() const;
+
+private:
+    CUmodule module_;
+    std::string log_msg_;
+    std::string error_msg_;
+};
+
+using device_ptr = CUdeviceptr;
+
+device_ptr device_malloc(std::size_t size);
+void device_free(device_ptr ptr);
+
+void memcpy(void* dst, device_ptr src, std::size_t size);
+void memcpy(device_ptr dst, void* src, std::size_t size);
+
+class unique_device_ptr
+{
+public:
+    unique_device_ptr(device_ptr ptr_);
+
+    unique_device_ptr(const unique_device_ptr&) = delete;
+    unique_device_ptr& operator=(const unique_device_ptr&) = delete;
+
+    unique_device_ptr(unique_device_ptr&& other);
+
+    unique_device_ptr& operator=(unique_device_ptr&& other);
+
+    ~unique_device_ptr();
+
+    device_ptr get();
+
+private:
+    device_ptr ptr_;
+};
+}
+}
+
+#endif
---
+++ qubus/src/cuda/core.cpp
@@ -0,0 +1,416 @@
+#include <qubus/cuda/core.hpp>
+
+#include <qubus/util/assert.hpp>
+
+#include <cstring>
+#include <utility>
+
+namespace qubus
+{
+namespace cuda
+{
+
+cuda_error::cuda_error(CUresult error_code_) : error_code_(error_code_)
+{
+}
+
+CUresult cuda_error::error_code() const
+{
+    return error_code_;
+}
+
+const char* cuda_error::what() const noexcept
+{
+    const char* msg;
+
+    check_cuda_error(cuGetErrorString(error_code(), &msg));
+
+    return msg;
+}
+
+void check_cuda_error(CUresult result)
+{
+    if (result != CUDA_SUCCESS)
+        throw cuda_error(result);
+}
+
+void init()
+{
+    check_cuda_error(cuInit(0));
+}
+
+device::device(int ordinal)
+{
+    check_cuda_error(cuDeviceGet(&device_, ordinal));
+}
+
+device::device(device&& other) : device_(std::move(other.device_))
+{
+    other.device_ = 0;
+}
+
+device& device::operator=(device&& other)
+{
+    device_ = std::move(other.device_);
+    other.device_ = 0;
+
+    return *this;
+}
+
+std::string device::name() const
+{
+    std::vector<char> id(100);
+
+    check_cuda_error(cuDeviceGetName(id.data(), id.size(), device_));
+
+    return std::string(id.data());
+}
+
+std::size_t device::total_mem() const
+{
+    std::size_t value;
+
+    check_cuda_error(cuDeviceTotalMem(&value, device_));
+
+    return value;
+}
+
+compute_capability_info device::compute_capability() const
+{
+    int major_revision;
+    int minor_revision;
+
+    check_cuda_error(cuDeviceComputeCapability(&major_revision, &minor_revision, device_));
+
+    return compute_capability_info(major_revision, minor_revision);
+}
+
+int device::get_attribute(CUdevice_attribute attribute) const
+{
+    int value;
+
+    check_cuda_error(cuDeviceGetAttribute(&value, attribute, device_));
+
+    return value;
+}
+
+int device::max_threads_per_block() const
+{
+    return get_attribute(CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK);
+}
+
+int device::max_shared_memory_per_block() const
+{
+    return get_attribute(CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK);
+}
+
+int device::warp_size() const
+{
+    return get_attribute(CU_DEVICE_ATTRIBUTE_WARP_SIZE);
+}
+
+int device::max_block_dim_x() const
+{
+    return get_attribute(CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X);
+}
+
+int device::max_grid_dim_x() const
+{
+    return get_attribute(CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X);
+}
+
+CUdevice device::native_handle() const
+{
+    return device_;
+}
+
+int get_device_count()
+{
+    int count;
+
+    check_cuda_error(cuDeviceGetCount(&count));
+
+    return count;
+}
+
+std::vector<device> get_devices()
+{
+    auto count = get_device_count();
+
+    std::vector<device> devices;
+    devices.reserve(count);
+
+    for (decltype(count) i = 0; i < count; ++i)
+    {
+        devices.push_back(device(i));
+    }
+
+    return devices;
+}
+
+context::context() : context_(0)
+{
+}
+
+context::context(const device& dev)
+{
+    check_cuda_error(cuCtxCreate(&context_, CU_CTX_SCHED_BLOCKING_SYNC, dev.native_handle()));
+}
+
+context::context(context&& other) : context_(std::move(other.context_))
+{
+    other.context_ = 0;
+}
+
+context& context::operator=(context&& other)
+{
+    context_ = std::move(other.context_);
+    other.context_ = 0;
+
+    return *this;
+}
+
+context::~context()
+{
+    if (context_ != 0)
+    {
+        cuCtxDestroy(context_);
+    }
+}
+
+CUcontext context::native_handle() const
+{
+    return context_;
+}
+
+namespace this_context
+{
+void synchronize()
+{
+    check_cuda_error(cuCtxSynchronize());
+}
+}
+
+namespace
+{
+void stream_callback_trampoline(CUstream this_stream, CUresult error_code, void* user_data)
+{
+    stream::callback_closure& callback_closure = *static_cast<stream::callback_closure*>(user_data);
+
+    callback_closure();
+}
+}
+
+stream::stream()
+{
+    check_cuda_error(cuStreamCreate(&stream_handle_, CU_STREAM_DEFAULT));
+}
+
+stream::stream(stream&& other)
+: stream_handle_(std::move(other.stream_handle_)),
+  pending_callbacks_(std::move(other.pending_callbacks_))
+{
+    other.stream_handle_ = 0;
+}
+
+stream& stream::operator=(stream&& other)
+{
+    stream_handle_ = std::move(other.stream_handle_);
+    other.stream_handle_ = 0;
+    pending_callbacks_ = std::move(other.pending_callbacks_);
+
+    return *this;
+}
+
+stream::~stream()
+{
+    if (stream_handle_ != 0)
+    {
+        cuStreamSynchronize(stream_handle_);
+        cuStreamDestroy(stream_handle_);
+    }
+
+    QUBUS_ASSERT(pending_callbacks_.empty(), "There are still pending callbacks.");
+}
+
+void stream::add_callback(std::function<void()> callback)
+{
+    auto new_pending_callback_position = pending_callbacks_.emplace(end(pending_callbacks_));
+    *new_pending_callback_position = [=] {
+        callback();
+        pending_callbacks_.erase(new_pending_callback_position);
+    };
+
+    check_cuda_error(cuStreamAddCallback(stream_handle_, stream_callback_trampoline,
+                                         &*new_pending_callback_position, 0));
+}
+
+CUstream stream::native_handle() const
+{
+    return stream_handle_;
+}
+
+function::function(CUfunction function_) : function_(function_)
+{
+}
+
+CUfunction function::native_handle() const
+{
+    return function_;
+}
+
+int calculate_max_active_blocks_per_multiprocessor(const function& kernel, int block_size,
+                                                   std::size_t dynamic_shared_mem_size)
+{
+    int num_blocks;
+
+    check_cuda_error(cuOccupancyMaxActiveBlocksPerMultiprocessor(
+        &num_blocks, kernel.native_handle(), block_size, dynamic_shared_mem_size));
+
+    return num_blocks;
+}
+
+suggested_launch_config
+calculate_launch_config_with_max_occupancy(const function& kernel,
+                                           std::size_t dynamic_shared_mem_size)
+{
+    int min_grid_size;
+    int block_size;
+
+    check_cuda_error(cuOccupancyMaxPotentialBlockSize(
+        &min_grid_size, &block_size, kernel.native_handle(), 0, dynamic_shared_mem_size, 0));
+
+    return suggested_launch_config(min_grid_size, block_size);
+}
+
+module::module(const std::string& ptx_code)
+{
+    static_assert(sizeof(std::uintptr_t) == sizeof(void*),
+                  "sizeof(std::uintptr_t) == sizeof(void*) must hold");
+
+    std::uintptr_t log_buffer_size = 8000;
+    std::uintptr_t error_buffer_size = 8000;
+
+    std::vector<char> log_buffer(log_buffer_size);
+    std::vector<char> error_buffer(error_buffer_size);
+
+    CUjit_option options[] = {CU_JIT_INFO_LOG_BUFFER_SIZE_BYTES, CU_JIT_INFO_LOG_BUFFER,
+                              CU_JIT_ERROR_LOG_BUFFER_SIZE_BYTES, CU_JIT_ERROR_LOG_BUFFER};
+
+    void* options_values[] = {reinterpret_cast<void*>(log_buffer_size), log_buffer.data(),
+                              reinterpret_cast<void*>(error_buffer_size), error_buffer.data()};
+
+    check_cuda_error(cuModuleLoadDataEx(&module_, ptx_code.c_str(), 4, options, options_values));
+
+    std::memcpy(&log_buffer_size, &options_values[0], sizeof(std::uintptr_t));
+    std::memcpy(&error_buffer_size, &options_values[2], sizeof(std::uintptr_t));
+
+    log_msg_ = std::string(log_buffer.data(), log_buffer_size);
+    error_msg_ = std::string(error_buffer.data(), error_buffer_size);
+}
+
+module::module(module&& other) : module_(std::move(other.module_))
+{
+    other.module_ = 0;
+}
+
+module& module::operator=(module&& other)
+{
+    module_ = std::move(other.module_);
+    other.module_ = 0;
+
+    return *this;
+}
+
+module::~module()
+{
+    if (module_ != 0)
+    {
+        cuModuleUnload(module_);
+    }
+}
+
+const std::string& module::log_message() const
+{
+    return log_msg_;
+}
+
+const std::string& module::error_message() const
+{
+    return error_msg_;
+}
+
+function module::get_function(const std::string& name) const
+{
+    CUfunction fn;
+    check_cuda_error(cuModuleGetFunction(&fn, native_handle(), name.c_str()));
+
+    return function(fn);
+}
+
+CUmodule module::native_handle() const
+{
+    return module_;
+}
+
+device_ptr device_malloc(std::size_t size)
+{
+    device_ptr ptr;
+
+    check_cuda_error(cuMemAlloc(&ptr, size));
+
+    return ptr;
+}
+
+void device_free(device_ptr ptr)
+{
+    if (ptr != 0)
+    {
+        check_cuda_error(cuMemFree(ptr));
+    }
+}
+
+void memcpy(void* dst, device_ptr src, std::size_t size)
+{
+    check_cuda_error(cuMemcpyDtoH(dst, src, size));
+}
+
+void memcpy(device_ptr dst, void* src, std::size_t size)
+{
+    check_cuda_error(cuMemcpyHtoD(dst, src, size));
+}
+
+unique_device_ptr::unique_device_ptr(device_ptr ptr_) : ptr_(ptr_)
+{
+}
+
+unique_device_ptr::unique_device_ptr(unique_device_ptr&& other) : ptr_(std::move(other.ptr_))
+{
+    other.ptr_ = 0;
+}
+
+unique_device_ptr& unique_device_ptr::operator=(unique_device_ptr&& other)
+{
+    ptr_ = std::move(other.ptr_);
+    other.ptr_ = 0;
+
+    return *this;
+}
+
+unique_device_ptr::~unique_device_ptr()
+{
+    try
+    {
+        device_free(ptr_);
+    }
+    catch (...)
+    {
+    }
+}
+
+device_ptr unique_device_ptr::get()
+{
+    return ptr_;
+}
+}
+}
