cd7d76cf3c3b143211ee878f3df550212d6b2add
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2017-05-01 23:18:45 +0200

    Fix the expression equality benchmark

--- qubus/benchmarks/expression_equality.cpp
+++ qubus/benchmarks/expression_equality.cpp
@@ -11,96 +11,114 @@ void simple_expression_tree_setup()
 {
     using namespace qubus;
 
-    expression expr = integer_literal_expr(42);
-    expression expr2 = integer_literal_expr(42);
+    auto expr = integer_literal(42);
+    auto expr2 = integer_literal(42);
 }
 
 void simple_expression_tree_compare_match()
 {
     using namespace qubus;
 
-    expression expr = integer_literal_expr(42);
-    expression expr2 = integer_literal_expr(42);
+    auto expr = integer_literal(42);
+    auto expr2 = integer_literal(42);
 
-    expr == expr2;
+    *expr == *expr2;
 }
 
 void simple_expression_tree_compare_mismatch()
 {
     using namespace qubus;
 
-    expression expr = integer_literal_expr(42);
-    expression expr2 = integer_literal_expr(7);
+    auto expr = integer_literal(42);
+    auto expr2 = integer_literal(7);
 
-    expr == expr2;
+    *expr == *expr2;
 }
 
 void complex_expression_tree_setup()
 {
     using namespace qubus;
 
-    variable_declaration var1(types::array(types::double_{}));
-    variable_declaration var2(types::array(types::double_{}));
+    variable_declaration var1(types::array(types::double_{}, 2));
+    variable_declaration var2(types::array(types::double_{}, 2));
 
     variable_declaration i(types::integer{});
     variable_declaration j(types::integer{});
 
-    auto body = binary_operator_expr(binary_op_tag::plus, subscription_expr(variable_ref_expr(var1), {variable_ref_expr(i), variable_ref_expr(j)}),
-                                     subscription_expr(variable_ref_expr(var2), {variable_ref_expr(i), variable_ref_expr(j)}));
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(i));
+    indices.push_back(var(j));
 
-    expression expr = for_expr(i, integer_literal_expr(0), integer_literal_expr(10), for_expr(j, integer_literal_expr(0), integer_literal_expr(10), body));
+    auto body =
+        subscription(var(var1), clone(indices)) + subscription(var(var2), clone(indices));
+
+    auto expr = for_(i, integer_literal(0), integer_literal(10),
+                     for_(j, integer_literal(0), integer_literal(10), std::move(body)));
 }
 
 void complex_expression_tree_compare_match()
 {
     using namespace qubus;
 
-    variable_declaration var1(types::array(types::double_{}));
-    variable_declaration var2(types::array(types::double_{}));
+    variable_declaration var1(types::array(types::double_{}, 2));
+    variable_declaration var2(types::array(types::double_{}, 2));
 
     variable_declaration i(types::integer{});
     variable_declaration j(types::integer{});
 
-    auto body = binary_operator_expr(binary_op_tag::plus, subscription_expr(variable_ref_expr(var1), {variable_ref_expr(i), variable_ref_expr(j)}),
-                                     subscription_expr(variable_ref_expr(var2), {variable_ref_expr(i), variable_ref_expr(j)}));
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(i));
+    indices.push_back(var(j));
 
-    expression expr = for_expr(i, integer_literal_expr(0), integer_literal_expr(10), for_expr(j, integer_literal_expr(0), integer_literal_expr(10), body));
+    auto body =
+            subscription(var(var1), clone(indices)) + subscription(var(var2), clone(indices));
 
-    expr == expr;
+    auto expr = for_(i, integer_literal(0), integer_literal(10),
+                     for_(j, integer_literal(0), integer_literal(10), std::move(body)));
+
+    *expr == *expr;
 }
 
 void complex_expression_tree_compare_early_mismatch()
 {
     using namespace qubus;
 
-    variable_declaration var1(types::array(types::double_{}));
-    variable_declaration var2(types::array(types::double_{}));
+    variable_declaration var1(types::array(types::double_{}, 2));
+    variable_declaration var2(types::array(types::double_{}, 2));
 
     variable_declaration i(types::integer{});
     variable_declaration j(types::integer{});
 
-    auto body = binary_operator_expr(binary_op_tag::plus, subscription_expr(variable_ref_expr(var1), {variable_ref_expr(i), variable_ref_expr(j)}),
-                                     subscription_expr(variable_ref_expr(var2), {variable_ref_expr(i), variable_ref_expr(j)}));
+    std::vector<std::unique_ptr<expression>> indices;
+    indices.push_back(var(i));
+    indices.push_back(var(j));
 
-    expression expr = for_expr(i, integer_literal_expr(0), integer_literal_expr(10), for_expr(j, integer_literal_expr(0), integer_literal_expr(10), body));
+    auto body =
+            subscription(var(var1), clone(indices)) + subscription(var(var2), clone(indices));
 
-    expression expr2 = integer_literal_expr(42);
+    auto expr = for_(i, integer_literal(0), integer_literal(10),
+                     for_(j, integer_literal(0), integer_literal(10), std::move(body)));
 
-    expr == expr2;
+    auto expr2 = integer_literal(42);
+
+    *expr == *expr2;
 }
 
-int main() {
+int main()
+{
     nonius::configuration cfg;
     cfg.output_file = "expression_equality.html";
 
     nonius::benchmark benchmarks[] = {
         nonius::benchmark("Simple expression tree setup", simple_expression_tree_setup),
-        nonius::benchmark("Simple expression tree compare (match)", simple_expression_tree_compare_match),
-        nonius::benchmark("Simple expression tree compare (mismatch)", simple_expression_tree_compare_mismatch),
+        nonius::benchmark("Simple expression tree compare (match)",
+                          simple_expression_tree_compare_match),
+        nonius::benchmark("Simple expression tree compare (mismatch)",
+                          simple_expression_tree_compare_mismatch),
         nonius::benchmark("Complex expression tree setup", complex_expression_tree_setup),
         nonius::benchmark("Complex expression tree compare (match)", complex_expression_tree_setup),
-        nonius::benchmark("Complex expression tree compare (early mismatch)", complex_expression_tree_setup)
-    };
+        nonius::benchmark("Complex expression tree compare (early mismatch)",
+                          complex_expression_tree_setup)};
 
     nonius::go(cfg, std::begin(benchmarks), std::end(benchmarks), nonius::html_reporter());
 
