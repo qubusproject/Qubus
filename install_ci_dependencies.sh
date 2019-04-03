d86bb5add9b3864dbfdd0e440b94955cff79776b
Christopher Hinz <chinz07@qubusproject.org>
2019-04-03 19:16:39 +0200

    Add the Qubus Project Conan repository

--- install_ci_dependencies.sh
+++ install_ci_dependencies.sh
@@ -6,4 +6,6 @@ apt install -y g++ python3 python3-pip
 
 pip3 install conan
 
+conan remote add qubusproject https://api.bintray.com/conan/qubusproject/conan
+
 conan install -g conan_paths -b outdated CI/
